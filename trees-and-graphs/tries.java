764cf6c556f430b3768ac91e2ccbfd9577f5ccc7
Linh Do <linhdo@umass.edu>
2016-07-13 19:56:59 +0700

    Update tries.java

--- trees-and-graphs/tries.java
+++ trees-and-graphs/tries.java
@@ -1,1 +1,11 @@
-private static
+private static class Node
+{
+    // each node has an array of links and a value
+    // characters are implicitly defined by link index
+    
+    private Object value;
+    private Node[] next = new Node[R];
+  
+    // Search hit. Need to examine all L characters for equality.
+    // Space. R null links at each leaf.
+}
