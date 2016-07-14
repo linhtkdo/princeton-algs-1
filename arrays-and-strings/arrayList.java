93e704a794ecc3c7b52a510935a34fb413d335f3
Linh Do <linhdo@umass.edu>
2016-07-14 13:32:06 +0700

    Update arrayList.java

--- arrays-and-strings/arrayList.java
+++ arrays-and-strings/arrayList.java
@@ -1,1 +1,35 @@
-
+public class ArrayListCustom<E>
+{
+  private static final int INITIAL_CAPACITY = 10;
+  private Object[] a;
+  private int N = 0;
+  
+  public ArrayListCustom()
+  { a = new Object[INITIAL_CAPACITY]; }
+  
+  public void add(E e)
+  {
+    if (N == a.length) resize();
+    a[N++] = e;
+  }
+  
+  public E get(int index)
+  {
+    if (index < 0 || index >= N) 
+      throw new IndexOutofBoundsException("Index: " + index + ", Size: " + index);
+    return (E) a[index];
+  }
+  
+  public Object remove(int index)
+  {
+    if (index < 0 || index >= N) 
+      throw new IndexOutofBoundsException("Index: " + index + ", Size: " + index);
+    
+    Object item = a[index];
+    for (int i=index; i<N; i++)
+      a[i] = a[i+1];
+    N--;
+    
+    return item;
+  }
+}
